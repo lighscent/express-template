95ca6db083b3f7bdb32fcb56f254da5d49732396
lighscent <augustin.pro.prs@gmail.com>
2026-03-03 19:17:46 +0100

    Add console log for server URL on startup

--- index.js
+++ index.js
@@ -27,4 +27,5 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
